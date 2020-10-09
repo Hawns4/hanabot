ebf57c0bb8c5f12cb15e47049e653a4f35705b6b
Hans Cabrera <hanscabrera04@gmail.com>
2020-10-09 17:43:37 -0600

    command handling

--- commands/hzv.js
+++ commands/hzv.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: "hzv",
     description: "Displays Hitzone Value",
+    args: true,
+    usage: "<Monster Name> <Damage Type>",
     execute(message, args) {
-        message.channel.send("Hitzone Values for " + args[1]);
+        message.channel.send("Hitzone Values for " + args[0]);
     },
 };
--- index.js
+++ index.js
@@ -5,32 +5,39 @@ const { prefix, token } = require("./config.json");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('/js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 commandFiles.forEach(file => {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
-})
-
-client.once("ready", () => {
-  console.log("Ready!");
 });
 
+client.on("ready", () => {
+  console.log("Ready!");
+});
 
 client.login(token);
 
 client.on("message", (message) => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(" ");
-  const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-  switch (command) {
-    case "hzv":
-      client.commands.get(command).execute(message, args);
-      break;
-    default:
-      message.channel.send("That isn't a command");
-      break;
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
+
+  if (command.args && !args.length) {
+    if (command.usage) {
+      return message.channel.send(`The proper syntax for \`${prefix}${command.name}\` is: \`${prefix}${command.name} ${command.usage}\``)
+    }
+  }
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command.');
   }
 
 });
