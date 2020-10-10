fbfa676a0527c90a7ff0a64084d6eb0dcde7b32d
Hans Cabrera <hanscabrera04@gmail.com>
2020-10-09 18:30:55 -0600

    library command update

--- commands/library.js
+++ commands/library.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "library",
     description: "shh",
+    args: false,
     execute(message, args) {
         message.channel.send("https://cdn.discordapp.com/emojis/454554627141009408.gif?v=1");
     }
