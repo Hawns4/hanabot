3e9d24804718e4cdf8ec3a319eb7e8ada336b510
Hans Cabrera <hanscabrera04@gmail.com>
2020-10-11 18:25:31 -0600

    embeds and monster data read

--- commands/hzv.js
+++ commands/hzv.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fs = require("fs");
 
 module.exports = {
     name: "hzv",
@@ -6,7 +7,6 @@ module.exports = {
     args: true,
     usage: "<Monster Name> <Damage Type>",
     execute(message, args) {
-        const fs = require("fs");
         const validDamageTypes = ["Sever", "Blunt", "Ranged", "Fire", "Water", "Thunder", "Ice", "Dragon"];
 
         for (let i = 0; i < args.length; i++) {
@@ -14,28 +14,25 @@ module.exports = {
         }
 
         try {
-            const damageType = args[args.length - 1];
+            let damageType = args[args.length - 1];
             if (validDamageTypes.includes(damageType)) {
                 args.pop();
-                const monsterName = args.join("_");
-                const monster = JSON.parse(fs.readFileSync(`./data/${monsterName}.json`));
-                //TODO: Send embedded hitzone values from JSON
-                return message.channel.send(createEmbed(monsterName, damageType));
             } else {
-                const monsterName = args.join("_");
-                const monster = JSON.parse(fs.readFileSync(`./data/${monsterName}.json`));
-                //TODO: Send embedded hitzone values from JSON
-                return message.channel.send(createEmbed(monsterName));
+                damageType = "Simple";
             }
+            const monsterName = args.join("_");
+            return message.channel.send(createEmbed(monsterName, damageType));
         } catch (err) {
+            console.log(err);
             message.channel.send("I couldn't find a monster by that name.\nPlease check the spelling of the name and try again.");
         }
     },
 };
 
 createEmbed = (monsterName, damageType) => {
+    let monsterData = JSON.parse(fs.readFileSync(`./data/${monsterName}.json`));
+    monsterData = monsterData[damageType];
     let embeddedData = new Discord.MessageEmbed();
-    embeddedData.attachFiles([`./data/${monsterName}.png`])
-    embeddedData.setThumbnail(`attachment://${monsterName}.png`);
+    embeddedData.setTitle(monsterName.split("_").join(" ")).attachFiles([`./data/${monsterName}.png`]).setThumbnail(`attachment://${monsterName}.png`);
     return embeddedData;
 }
