f93a07e92c471a652fd3c9a0a2619e8cb5e18cdb
Hans Cabrera <hanscabrera04@gmail.com>
2020-10-13 15:28:36 -0600

    github command added

---
+++ commands/github.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "github",
+    description: "Hana Bot's github repository",
+    args: false,
+    execute(message, args) {
+        message.channel.send("https://github.com/Hawns4/hanabot");
+    }
+}
